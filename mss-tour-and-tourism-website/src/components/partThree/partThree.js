0a2097c7768d1fd69410b5a43e19d52486295a7e
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-09-06 17:23:45 +0300

    intsall mui core

--- mss-tour-and-tourism-website/src/components/partThree/partThree.js
+++ mss-tour-and-tourism-website/src/components/partThree/partThree.js
@@ -1,5 +1,6 @@
 import { Box, Button, Grid, Stack } from "@mui/material";
 import  React ,{ useState, useEffect } from "react";
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 
 import img1 from '../../assets/hotels/sheraton.jpg'
@@ -16,15 +17,16 @@ const PartThree=()=>{
         <Stack direction='column' pl={12} pr={12} spacing={2}>
 
          <Stack  direction='row' spacing={2} sx={{ marginLeft:15 }}>
-            <Box sx={{ width:753,height:380, border:0.5 , borderRadius:'5%'}}>
+           <ButtonBase>
+             <Box sx={{ width:753,height:380, border:0.5 , borderRadius:'5%'}}>
                 <img src={img1} style={{width:753, height:380, border:0.5 , borderRadius:'5%' }}/>
-                <h1  style={{ position:'relative',bottom:400,left:330}}>Sheraton Hotel</h1>
+                <h1  style={{ position:'relative',bottom:400}}>Sheraton Hotel</h1>
 
-            </Box>
-
+             </Box>
+            </ButtonBase>
             <Box sx={{ width:754,height:380, border:0.5 , borderRadius:'5%'}}>
                 <img src={img1} style={{width:754, height:380, border:0.5 , borderRadius:'5%' }}/>
-                <h1  style={{ position:'relative',bottom:400,left:330}}>Sheraton Hotel</h1>
+                <h1  style={{ position:'relative',bottom:400,}}>Sheraton Hotel</h1>
 
             </Box> 
          </Stack>
