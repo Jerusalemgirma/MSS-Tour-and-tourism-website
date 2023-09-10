dec87ecd321d7e617f748ad5e67b3167e0e81200
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-09-11 02:47:32 +0300

    start unesico registerd events

--- mss-tour-and-tourism-website/src/components/partEight/partEight.js
+++ mss-tour-and-tourism-website/src/components/partEight/partEight.js
@@ -8,23 +8,29 @@ import img2 from '../../assets/unesico/meskel2.jpg'
 import img3 from '../../assets/unesico/meskel3.webp'
 import img4 from '../../assets/unesico/meskel4.jpg'
 import img5 from '../../assets/unesico/meskel5.jpg'
-
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import ButtonBase from "@mui/material";
 
 const PartEight=()=>{
 
     const Cardd=(props)=>{
         return(
-            <Stack direction='row'   p={3} sx={{boxShadow:4,backgroundColor:'#DDF9EE' }} >
-              <Box sx={{ width:650 }}>
-                  <img src={props.image1} style={{ width:640 ,height:800}}/>
+            <Stack direction='row'   p={2} sx={{boxShadow:4,backgroundColor:'#F3F7F9' }} >
+              <Box sx={{ width:650,marginRight:2 }}>
+                  <img src={props.image1} style={{ width:635 ,height:755}}/>
               </Box>
               <Box  sx={{ textAlign:"center"}}>
                 
                     <h1>{props.name}</h1>
                     <h6 style={{ fontSize:20 }}>{props.location}</h6>
-                    <p style={{ marginBottom:203 ,fontSize:30,marginLeft:30,marginRight:30}}>{props.detail}</p>
-                  <Stack direction='row' spacing={1}>
-                    <img src={props.image5} style={{ width:270,height:240 }}/>
+                    <p style={{ fontSize:30,marginLeft:30,marginRight:30,marginBottom:128}}>{props.detail}</p>
+                    <Stack   direction='row' ml={60}> 
+                      <Box  sx={{ border:3,borderRadius:'50%',width:40,height:40,paddingTop:2 ,paddingLeft:2}}><ArrowBackIosIcon /></Box>
+                      <Box  sx={{ border:3,borderRadius:'50%',width:40,height:40,paddingTop:2 ,paddingLeft:2}}><ArrowForwardIosSharpIcon  /></Box>
+                    </Stack>
+                  <Stack direction='row' spacing={1} >
+                    <img src={props.image5} style={{ width:265,height:240 }}/>
                     <img src={props.image3 } style={{ width:270,height:240 }}/>
                     <img src={props.image4} style={{ width:270,height:240 }}/>
                     <img src={props.image2} style={{ width:270,height:240}}/>
